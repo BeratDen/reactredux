aa2cbf8d6c4c41be1b8855f07c8ee73eccc7e1f6
Berat Deniz <berat.denizsids@gmail.com>
2023-01-05 23:06:10 +0300

    Message!! :)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { useEffect } from "react";
+import { PostService, UserService } from "./services";
+
 function App() {
-  return <>App</>;
+  useEffect(() => {
+    // Post Service
+    PostService.getPosts().then((res) => console.log(res));
+    PostService.getPostDetail(1).then((res) => console.log(res));
+    PostService.newPost({
+      userId: 3,
+      title: "test",
+      body: "test",
+    }).then((res) => console.log(res));
+    // Post Service
+
+    // User Service
+
+    UserService.getUser().then((res) => console.log(res));
+    UserService.getuserDetail(3).then((res) => console.log(res));
+    UserService.newUser({
+      name: "berat",
+      userName: "elite",
+      email: "berat.denizsids@gmail.com",
+    });
+
+    // User Service
+  }, []);
+
+  return <></>;
 }
 
 export default App;
